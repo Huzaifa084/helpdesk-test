836d924101784ff25794a91af1e6c583f7c6c576
Muhammad Abdullah Awais <abdullahawais1111@gmail.com>
2025-06-18 00:23:33 +0500

    fix: add background color to app header

--- desk/src/components/layouts/AppHeader.vue
+++ desk/src/components/layouts/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex border-b pr-5">
-    <div id="app-header" class="flex-1 w-full"></div>
+    <div id="app-header" class="flex-1 w-full bg-blue-100"></div>
   </div>
 </template>
 
